debfb460e04a57639a708745f1f7ccf87d7d3f6f
Antonio Lima <a.m.maiadelima@gmail.com>
2017-02-13 15:06:32 +0000

    1st rails exercise done

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,21 @@
+class CoachingController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    elsif your_message == "I am going to work right now!"
+      return ""
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
